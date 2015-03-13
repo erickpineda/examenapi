1411b64fa8757cb12a28978a2c64ded86ac0d964
erickpineda <epcloud2012@gmail.com>
2015-03-13 21:14:47 +0000

    Commit fuera de tiempo, editado fichero server-*

--- server-producteAngular.js
+++ server-producteAngular.js
@@ -6,11 +6,11 @@ app.use(bodyParser.json());
 app.use("/api/productes", require("./controllers/api/productes"));
 app.use("/",require("./controllers/static"));
 
-app.listen(8080,function(){
+/*app.listen(8080,function(){
     console.log("Servidor funcionando por el puerto 8080");
-});
+});*/
 
-/*
+
 app.listen(process.env.PORT, function() {
     console.log('Server listening on', process.env.PORT);
-});*/
+});
